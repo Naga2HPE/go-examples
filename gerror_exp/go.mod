c6b2969fafa6f8c91ac68eca8d114ab21b59db46
Nagarjuna S <nagarjuna.sarvepalli@hpe.com>
2022-03-07 11:06:29 +0530

    gerror example

---
+++ gerror_exp/go.mod
@@ -0,0 +1,5 @@
+module github.com/Naga2HPE/go-examples/gerror_exp
+
+go 1.17
+
+require github.com/cf-guardian/guardian v0.0.0-20151111082959-80736895e96f
---
+++ gerror_exp/main.go
@@ -0,0 +1,29 @@
+// (C) Copyright 2021-2022 Hewlett Packard Enterprise Development LP
+// Package gerror_exp contains ...
+package main
+
+import (
+	"fmt"
+
+	"github.com/cf-guardian/guardian/gerror"
+)
+
+/*
+Author : Nagarjuna S
+Date : 07/03/22 10:47 AM
+Project : gerror_exp
+File : main.go
+*/
+
+func main() {
+
+	fmt.Printf("%v", first())
+}
+
+func first() error {
+	return second()
+}
+
+func second() error {
+	return gerror.New("ErrExample", "Example error message")
+}
